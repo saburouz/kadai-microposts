5eb92c2bc3cc74aa43dcfd01f2d0ea5953db373c
saburouz <saburouz.public@gmail.com>
2019-07-11 11:27:29 +0000

    kadai-microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   
   validates :content, presence: true, length: { maximum: 255 }
+  
+  has_many :reverses_of_favorite, class_name: 'Relationship', foreign_key: 'micropost_id'
+  has_many :likers, through: :reverses_of_favorite, source: :user
 end
